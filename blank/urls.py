7e808e8a07d1751e20bb0f0e35bde3ae02471dde
zarechenec <zarechenec@osla.ru>
2018-04-19 11:57:31 +0300

    end #4

--- blank/urls.py
+++ blank/urls.py
@@ -18,3 +18,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
--- snippets/rest/v1/views.py
+++ snippets/rest/v1/views.py
@@ -1,42 +1,31 @@
-from snippets.models import Snippet
-from .serializers import SnippetSerializer
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
+from snippets.models import Snippet
+from .serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
-#  class SnippetList(mixins.ListModelMixin,
-#                    mixins.CreateModelMixin,
-#                    generics.GenericAPIView):
-#      queryset = Snippet.objects.all()
-#      serializer_class = SnippetSerializer
-#
-#      def get(self, request, *args, **kwargs):
-#          return self.list(request, *args, **kwargs)
-#
-#      def post(self, request, *args, **kwargs):
-#          return self.create(request, *args, **kwargs)
-#
-#
-#  class SnippetDetail(mixins.RetrieveModelMixin,
-#                      mixins.UpdateModelMixin,
-#                      mixins.DestroyModelMixin,
-#                      generics.GenericAPIView):
-#      queryset = Snippet.objects.all()
-#      serializer_class = SnippetSerializer
-#
-#      def get(self, request, *args, **kwargs):
-#          return self.retrieve(request, *args, **kwargs)
-#
-#      def put(self, request, *args, **kwargs):
-#          return self.update(request, *args, **kwargs)
-#
-#      def delete(self, request, *args, **kwargs):
-#          return self.destroy(request, *args, **kwargs)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
